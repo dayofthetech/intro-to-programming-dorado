e1e14378196cabdbc405501dd80f69801326218b
dayofthetech <j.alberto.reyes.r90@gmail.com>
2022-10-21 21:34:49 -0500

    clean up code

--- odinProjectForms/forms.js
+++ odinProjectForms/forms.js
@@ -11,25 +11,14 @@ const messagesDisplay = document.getElementById('messages-box');
 
 btn.addEventListener('click', function(e){
     e.preventDefault();
+
     //.value without the parenthesis is a method
     //with parenthesis is a function and the code wont work
     const firstName = document.getElementById('first_name').value
-    const lastName = document.getElementById('last_name').value
     const email = document.getElementById('email').value
-    //this works, now I need to append it to the empty field as an li element
-    //this won't work outside the function
-    // console.log(firstName);
-    // console.log(lastName);
-    // console.log(email);
+    const userMessage = document.getElementById('user-message').value
+
     const message = document.createElement('li');
-    //this display as <li>a,r,email@email.com</li>
-    message.innerHTML = [firstName, lastName, email];
-    messagesDisplay.appendChild(message);
-
-
-    //below - undefined
-    //console.log(message[1]);
-
-
-
+    message.innerHTML = [firstName, email, userMessage];
+    messagesDisplay.append(message);
 })
