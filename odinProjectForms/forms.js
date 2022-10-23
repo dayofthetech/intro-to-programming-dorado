0df2dae3177a8b97b8de1a5613a4aa81aeae2e76
dayofthetech <j.alberto.reyes.r90@gmail.com>
2022-10-23 13:44:14 -0500

    comple form, but I'm not sure if it goes to main

--- odinProjectForms/forms.js
+++ odinProjectForms/forms.js
@@ -5,20 +5,62 @@
 // })
 
 
+// const btn = document.getElementById('btn');
+// const messagesDisplay = document.getElementById('messages-box');
+
+// btn.addEventListener('click', function(e){
+//     e.preventDefault();
+
+//     //.value without the parenthesis is a method
+//     //with parenthesis is a function and the code wont work
+//     const firstName = document.getElementById('first_name').value
+//     const email = document.getElementById('email').value
+//     const userMessage = document.getElementById('user-message').value
+
+//     const message = document.createElement('li');
+//     message.innerHTML = [firstName, email, userMessage];
+//     messagesDisplay.append(message);
+// })
+
+//main action of button
 const btn = document.getElementById('btn');
 //global ul element
-const messagesDisplay = document.getElementById('messages-box');
+const messagesDisplay = document.getElementById('messages_box');
+
 
 btn.addEventListener('click', function(e){
     e.preventDefault();
 
-    //.value without the parenthesis is a method
-    //with parenthesis is a function and the code wont work
+    //grab the values from each form field
     const firstName = document.getElementById('first_name').value
     const email = document.getElementById('email').value
-    const userMessage = document.getElementById('user-message').value
+    const userMessage = document.getElementById('user_message').value
 
+    //create the elements that will go inside the html
     const message = document.createElement('li');
-    message.innerHTML = [firstName, email, userMessage];
+    const link = document.createElement('a');
+    const span = document.createElement('span');
+
+    //create button to remove the message
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.setAttribute('type', 'button');
+    removeBtn.addEventListener('click', function(){
+        messagesDisplay.removeChild(message);
+    })
+
+    //create button to edit message
+
+    //format how the elements will look like
+    span.innerHTML = ` wrote ${userMessage} `;
+    link.href = email;
+    link.innerHTML = firstName;
+
+    message.append(link);
+    message.append(span);
+    message.append(removeBtn);
     messagesDisplay.append(message);
+
+    const messageForm = document.getElementById('leave_message').reset();
+
 })
